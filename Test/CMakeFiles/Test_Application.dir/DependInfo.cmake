188fedbbecd6c2ed3829cbb4b24c0a97bb8a3de2
joshir14 <joshir14@coventry.ac.uk>
2023-12-14 00:49:14 +0530

    Unit testing

---
+++ Test/CMakeFiles/Test_Application.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/pi/Desktop/7166CEM/Test/Test_Application.c" "/home/pi/Desktop/7166CEM/Test/CMakeFiles/Test_Application.dir/Test_Application.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "Include"
+  "Src_autogen"
+  "Test/Unity"
+  "Test/Unity/unity"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Test/CMakeFiles/Test_Application.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Test_Application.dir/Test_Application.c.o"
+  "bin/Test_Application.pdb"
+  "bin/Test_Application"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Test_Application.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
